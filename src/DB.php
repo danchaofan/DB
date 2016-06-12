2cd6e810f8a2370d2a80481f6617847cb71e179a
wangdong <wangdong@mydaydream.com>
2016-06-12 10:53:11 +0800

    dev

---
+++ src/DB.php
@@ -0,0 +1,22 @@
+<?php
+/***************************************************************************
+ *
+ * copyright Copyright (c) 2016 mydaydream.com, Inc. All Rights Reserved
+ *
+ **************************************************************************/
+
+
+
+/**
+ * @file DB.php
+ * @author wangdong <wangdong@mydaydream.com>
+ * @date 2016年6月12日
+ * @description 
+ * @version 1.0
+ **/
+class DB{
+    
+    public static function test(){
+        echo "test";
+    }
+}
